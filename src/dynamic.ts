697e7cfac13d774000c6f950f9c2bd77914557af
Zachary <zsakowitz@zsnout.com>
2022-09-19 18:08:20 -0700

    Add return type

--- src/dynamic.ts
+++ src/dynamic.ts
@@ -4,7 +4,7 @@ export function Dynamic({
   children: get,
 }: {
   children: Accessor<JSX.Element>;
-}) {
+}): JSX.Element {
   let node = untrack(get);
   createEffect(() => node.replaceWith((node = get())));
 
