bcb598fdbfe63b1d7c5c9047d2fe396436209fd1
Zachary <zsakowitz@zsnout.com>
2022-09-19 18:33:26 -0700

    Add dynamic and dynamic-list to willow/index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /// <reference path="./types.ts" />
 
+export * from "./dynamic-list.js";
+export * from "./dynamic.js";
 export * from "./jsx.js";
 export * from "./maybe.js";
 export * from "./reactivity.js";
