aa7930eb3cbb0f8910d247106395c619c4a9c798
Zachary <zsakowitz@zsnout.com>
2022-09-21 08:53:25 -0700

    Add For

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /// <reference path="./types.ts" />
 
 export * from "./dynamic.js";
+export * from "./for.js";
 export * from "./fragment.js";
 export * from "./jsx.js";
 export * from "./list.js";
