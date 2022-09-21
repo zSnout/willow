e4d7a8c7dd958d9ad64af2d3b1268a3ba19e7aa5
Zachary <zsakowitz@zsnout.com>
2022-09-21 08:53:30 -0700

    Use <For> in example

--- src/example.tsx
+++ src/example.tsx
@@ -1,4 +1,6 @@
+import { For } from "./for.js";
 import { h } from "./jsx.js";
+import { List } from "./list.js";
 import { Maybe } from "./maybe.js";
 import { createMemo, createReactive, createSignal } from "./reactivity.js";
 
@@ -6,7 +8,7 @@ const [age, setAge] = createSignal(0, { name: "age" });
 const [name, setName] = createSignal("Zachary", { name: "name" });
 const plural = createMemo(() => (age() === 1 ? "" : "s"), { name: "plural" });
 const isPlural = createMemo(() => age() !== 1, { name: "isPlural" });
-const nums = createReactive<JSX.Element[]>([]);
+const nums = createReactive<number[]>([]);
 
 document.body.appendChild(
   <>
@@ -21,8 +23,15 @@ document.body.appendChild(
       <p>not plural L</p>
     </Maybe>
 
-    <button on:click={() => nums.push(<p>{Math.random()}</p>)}>
-      ayo click me
-    </button>
+    <button on:click={() => nums.push(Math.random())}>ayo click me</button>
+
+    <For each={nums}>
+      {(num) => (
+        <>
+          <h2>main: {num}</h2>
+          <p>my id: {Math.random()}</p>
+        </>
+      )}
+    </For>
   </>
 );
