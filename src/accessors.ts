adb3f5fdd3b14d8b17ad30741d3f413e289af316
Zachary <zsakowitz@zsnout.com>
2022-09-22 07:00:01 -0700

    Add accessors.js

---
+++ src/accessors.ts
@@ -0,0 +1,64 @@
+import {
+  Accessor,
+  isAccessor,
+  isSignal,
+  Signal,
+  ValueOrAccessor,
+} from "./primitives.js";
+
+export type UnwrapNestedAccessors<T> = {
+  readonly [K in keyof T as T[K] extends Signal<any>
+    ? never
+    : K]: T[K] extends ValueOrAccessor<infer U> ? U : never;
+} & {
+  [K in keyof T as T[K] extends Signal<any> ? K : never]: T[K] extends Signal<
+    infer U
+  >
+    ? U
+    : never;
+};
+
+export type MaybeAccessors<T> = {
+  [K in keyof T]: T[K] extends Accessor<any>
+    ? T[K]
+    : T[K] extends Signal<any>
+    ? T[K]
+    : ValueOrAccessor<T[K]>;
+};
+
+export function toStore<T extends object>(
+  object: T
+): /** We use this format of UnwrapNestedAccessors to display the type better in IDEs. */ {
+  [K in keyof UnwrapNestedAccessors<T>]: UnwrapNestedAccessors<T>[K];
+} {
+  const result = Object.create(null);
+
+  for (const key in object) {
+    const value = object[key];
+
+    if (isAccessor(value)) {
+      Object.defineProperty(result, key, {
+        configurable: true,
+        enumerable: true,
+        get() {
+          return value();
+        },
+      });
+    } else if (isSignal(value)) {
+      Object.defineProperty(result, key, {
+        configurable: true,
+        enumerable: true,
+        get() {
+          return value[0]();
+        },
+        set(value) {
+          return value[1](value);
+        },
+      });
+    } else {
+      result[key] = value;
+    }
+  }
+
+  return result;
+}
