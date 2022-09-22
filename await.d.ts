bcb9307e6fa814b58c5703fe05328f50a11db144
zsakowitz <zsakowitz@users.noreply.github.com>
2022-09-22 23:11:43 +0000

    deploy: c6df35cea16cbf7d5361d9355f7c5888552014ea

--- await.d.ts
+++ await.d.ts
@@ -1,8 +1,7 @@
-import { WillowFragment } from "./fragment.js";
 import { ValueOrAccessor } from "./primitives.js";
 export declare function Await<T>({ value, pending, then, catch: c, }: {
     value: ValueOrAccessor<T>;
     pending?: JSX.Element;
     then?: (value: Awaited<T>) => JSX.Element;
     catch?: (error: unknown) => JSX.Element;
-}): WillowFragment;
+}): JSX.Element;
